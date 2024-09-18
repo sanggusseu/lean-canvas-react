057287a9306895e40d2de698056ae11b9a6d3a46
sanggusseu <sanggusseu@gmail.com>
2024-09-18 23:28:37 +0900

    타이틀 컴포넌트

---
+++ src/components/CanvasTitle.jsx
@@ -0,0 +1,54 @@
+import { useState } from 'react';
+import { FaCheck, FaEdit } from 'react-icons/fa';
+
+export default function CanvasTitle() {
+  const [title, setTitle] = useState('Lean Canvas');
+  const [editedTitle, setEditedTite] = useState(title);
+  const [isEditing, setIsEditing] = useState(false);
+
+  const handleEditTitle = () => {
+    setIsEditing(true);
+  };
+
+  const handleTitleChange = e => {
+    setEditedTite(e.target.value);
+  };
+
+  const handleTitleSubmit = () => {
+    setTitle(editedTitle);
+    setIsEditing(false);
+  };
+
+  return (
+    <div className="flex items-center justify-center mb-10">
+      {isEditing ? (
+        <div className="flex items-center">
+          <input
+            type="text"
+            className="text-4xl font-bold text-center text-blue-600 bg-transparent border-b-2 border-blue-600 focus:outline-none"
+            value={editedTitle}
+            onChange={handleTitleChange}
+          />
+          <button
+            className="ml-2 p-2 bg-green-500 text-white rounded-full hover:bg-green-600 transition duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50"
+            aria-label="Save title"
+            onClick={handleTitleSubmit}
+          >
+            <FaCheck />
+          </button>
+        </div>
+      ) : (
+        <>
+          <h1 className="text-4xl font-bold text-center ">{title}</h1>
+          <button
+            className="ml-2 p-2 bg-yellow-500 text-white rounded-full hover:bg-yellow-600 transition duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:ring-opacity-50"
+            aria-label="Edit title"
+            onClick={handleEditTitle}
+          >
+            <FaEdit />
+          </button>
+        </>
+      )}
+    </div>
+  );
+}
