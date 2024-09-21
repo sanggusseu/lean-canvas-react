140ac48aa47713934c4b781c55bf9b3cada461a6
sanggusseu <sanggusseu@gmail.com>
2024-09-21 14:26:47 +0900

    검색 필터 리뉴얼

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,17 +7,27 @@ import Loading from '../components/Loading';
 import Error from '../components/Error';
 import Button from '../components/Button';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import CategoryFilter from '../components/CategoryFilter';
 
 function Home() {
-  const [searchText, setSearchText] = useState();
+  const [filter, setFilter] = useState({
+    searchText: undefined,
+    category: undefined,
+  });
+  const handleFilter = (key, value) =>
+    setFilter({
+      ...filter,
+      [key]: value,
+    });
   const [isGridView, setIsGridView] = useState(true);
 
   const queryClient = useQueryClient();
 
   // 1] 데이터 조회
   const { data, isLoading, error, refetch } = useQuery({
-    queryKey: ['canvases', searchText],
-    queryFn: () => getCanvases({ title_like: searchText }),
+    queryKey: ['canvases', filter.searchText, filter.category],
+    queryFn: () =>
+      getCanvases({ title_like: filter.searchText, category: filter.category }),
     initialData: [],
   });
 
@@ -49,7 +59,16 @@ function Home() {
   return (
     <>
       <div className="mb-6 flex flex-col sm:flex-row items-center justify-between">
-        <SearchBar searchText={searchText} setSearchText={setSearchText} />
+        <div className="flex gap-2 flex-col w-full sm:flex-row mb-4 sm:mb-0">
+          <SearchBar
+            searchText={filter.searchText}
+            onSearch={val => handleFilter('searchText', val)}
+          />
+          <CategoryFilter
+            category={filter.category}
+            onChange={val => handleFilter('category', val)}
+          />
+        </div>
         <ViewToggle isGridView={isGridView} setIsGridView={setIsGridView} />
       </div>
       <div className="flex justify-end mb-6">
@@ -63,7 +82,7 @@ function Home() {
         <CanvasList
           filteredData={data}
           isGridView={isGridView}
-          searchText={searchText}
+          searchText={filter.searchText}
           onDeleteItem={handleDeleteItem}
         />
       )}
